c111962f6ab7c651ea566fb4b8feb9c57674fca5
memosk8 <memolopez9363@gmail.com>
2022-08-22 19:28:55 -0500

    added user reference to goal Schema

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose'),
   goalSchema = mongoose.Schema({
+    // reference to a user
+    user:{
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
     text: {
       type: String,
       required: [true, 'Please add a text value']
