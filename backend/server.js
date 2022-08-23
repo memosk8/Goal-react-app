ef1398e8bf1c203b5db67d0c82f63db98837bc32
memosk8 <memolopez9363@gmail.com>
2022-08-22 19:29:16 -0500

    included user routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,6 @@ app
   .use(express.json())
   .use(express.urlencoded({extended: false}))
   .use('/api/goals', require('./routes/goalRoutes')) // rutas para goals
+  .use('/api/users', require('./routes/userRoutes')) // rutas para users
   .use(errorHandler)
   .listen(port, () => console.log(`server started on port : ${port}`));
