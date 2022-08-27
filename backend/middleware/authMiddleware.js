73271e4a2907fb4b28bc87ddd0a63209f1575ead
memosk8 <memolopez9363@gmail.com>
2022-08-27 16:13:09 -0500

    validate user from the bearer token OK

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,24 +1,35 @@
-const jwt = require('jsonwebtoken'),
-  asyncHandler = require('express-async-handler'),
-  User = require('../models/userModel'),
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
-  protect = asyncHandler((req, res, next) => {
-    let token
+const protect = asyncHandler(async (req, res, next) => {
+  let token
 
-    if (req.headers.authorization && req.header.authorization.startsWith('Bearer')) {
-      try {
-        token = req.headers.authorization.split(' ')[1]
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
 
-        // verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-        // get user from generateToken
-        req.user = await User.findById(decoded.id).select('-password')
-        next();
-      } catch (error) {
-        console.log(error)
-        res.status(401)
-        throw new Error('Not authorized')
-      }
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password')
+
+      next()
+    } catch (error) {
+      res.status(401)
+      throw new Error('Not authorized')
     }
-  })
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
+  }
+})
+
+module.exports = { protect }
