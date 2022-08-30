087df75a63a6924cdff9b1dd496289a52292d3fc
memosk8 <memolopez9363@gmail.com>
2022-08-30 13:13:09 -0500

    added react-router-dom to react frontend project

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,6 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   const { _id, name, email } = await User.findById(req.user.id)
 
-
   res.status(200).json({
     id: _id,
     name, email
