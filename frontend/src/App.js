01de741d56c3599e4076ebe6b7b9c821738fd1d0
memosk8 <memolopez9363@gmail.com>
2022-09-01 11:34:31 -0500

    added login, register and dashboard pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <>
       <Router>
         <div className='container'>
-          <h1>My app french</h1>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
         </div>
       </Router>
     </>
