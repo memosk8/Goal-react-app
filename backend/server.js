73dc7f23849178856a0b86915fc2a61266f02582
/home/memosk8 <memolopez9363@gmail.com>
2022-09-02 12:27:51 -0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
-const express = require("express"),
-  colors = require('colors'),
-  dotenv = require("dotenv").config(),
-  {errorHandler} = require('./middleware/errorMiddleware'),
-  connectDB = require('./config/db'),
-  port = process.env.PORT,
-  app = express();
+const express = require("express"),       // Handle HTTP req/res with Node.js
+  colors = require('colors'),             // Print to console with styles
+  dotenv = require("dotenv").config(),    // Handle env variables
+  {errorHandler} = require('./middleware/errorMiddleware'), // Custom middleware
+  connectDB = require('./config/db'),     // Mongodb config
+  port = process.env.PORT,                // Server Port
+  app = express();                        // Express app initialization
 
 connectDB();
 
+// server config
 app
   .use(express.json())
   .use(express.urlencoded({extended: false}))
-  .use('/api/goals', require('./routes/goalRoutes')) // rutas para goals
-  .use('/api/users', require('./routes/userRoutes')) // rutas para users
+  .use('/api/goals', require('./routes/goalRoutes')) 
+  .use('/api/users', require('./routes/userRoutes')) 
   .use(errorHandler)
   .listen(port, () => console.log(`server started on port : ${port}`));
