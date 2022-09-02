fdb7575ef4198e2851d2b0628d4508316cbf137b
/home/memosk8 <memolopez9363@gmail.com>
2022-09-02 13:21:57 -0500

    Installed styled-components dependency Created footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import React from 'react';
 
 function App() {
   return (
     <>
       <Router>
         <div className='container'>
-          <Header/>
+          <Header />
           <Routes>
             <Route path='/' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
           </Routes>
+
         </div>
+        <Footer />
       </Router>
     </>
   );
---
+++ frontend/src/components/FooterStyles.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+padding: 70px 60px;
+background: black;
+position: relative;
+
+width: 100%;
+maring-top: 20px;
+
+@media (max-width: 1000px) {
+	padding: 70px 30px;
+}
+`;
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	max-width: 1000px;
+	margin: auto auto;
+	/* background: red; */
+`
+
+export const Column = styled.div`
+display: flex;
+flex-direction: column;
+text-align: left;
+margin-left: 60px;
+`;
+
+export const Row = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fill,
+						minmax(185px, 1fr));
+grid-gap: 20px;
+
+@media (max-width: 1000px) {
+	grid-template-columns: repeat(auto-fill,
+						minmax(200px, 1fr));
+}
+`;
+
+export const FooterLink = styled.a`
+color: #fff;
+margin-bottom: 20px;
+font-size: 18px;
+text-decoration: none;
+text-align: center;
+
+&:hover {
+	color: teal;
+	transition: 400ms ease-in;
+}
+`;
+
+export const Heading = styled.p`
+font-size: 24px;
+color: #fff;
+margin: 30px 0;
+font-weight: bold;
+text-align: center;
+`;
